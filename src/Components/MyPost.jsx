b478462eadc5e144a18db167aa726bee1e53eda4
harmindersaini94 <harminder_saini94@outlook.com>
2024-08-09 16:59:00 -0700

    All_Page_UI_Done

--- src/Components/MyPost.jsx
+++ src/Components/MyPost.jsx
@@ -15,16 +15,30 @@ const MyPost = () => {
   },[]);
 
   return (
-    <div className="w-full py-8">
-      <div className="flex flex-wrap">
-        {allPostData.map((post) => (
-          <div key={post.$id} className="p-8 w-1/4">
-            {console.log("post here", post)}
-            <Card {...post} />
-          </div>
-        ))}
-      </div>
+    <div className="text-center my-4 lg:my-8 overflow-hidden mx-auto"> 
+    <h2 className="text-4xl mb-12 font-bold text-cyan-600">My Stories</h2>
+    <div className="w-full lg:w-full flex flex-wrap justify-center items-center gap-8 content-center">
+      {allPostData.map(
+        (post, index) =>
+          index < 3 && (
+            <div key={post.$id} className="">
+              <Card {...post} />
+            </div>
+          )
+      )}
+      
     </div>
+  </div>
+    // <div className="w-full py-8">
+    //   <div className="flex flex-wrap">
+    //     {allPostData.map((post) => (
+    //       <div key={post.$id} className="p-8 w-1/4">
+    //         {console.log("post here", post)}
+    //         <Card {...post} />
+    //       </div>
+    //     ))}
+    //   </div>
+    // </div>
   );
 }
 
